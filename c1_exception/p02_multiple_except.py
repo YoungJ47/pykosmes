69a730c49335011ac8a4efd1165904d28474ee2b
YounghoJung <wjddudgh47@gmail.com>
2023-09-13 14:44:54 +0900

    2023.09.13 오후 학습 내용

--- c1_exception/p02_multiple_except.py
+++ c1_exception/p02_multiple_except.py
@@ -2,9 +2,9 @@ my_list = [1, 2, 3]
 try:
     print("my_list = [1, 2, 3]")
     index = int(input("첨자를 입력하세요: "))
-    #print(my_list[index]/0)
-    for i in range(100000):
-        print(i, end=' ')
+    print(my_list[index]/0)
+    #for i in range(100000):
+    #    print(i, end=' ')
 except ZeroDivisionError:
     print("0으로 나눌 수 없습니다.")
 except IndexError:
@@ -13,4 +13,3 @@ except ValueError:
     print("숫자를 입력하세요")
 except KeyboardInterrupt:
     print("KeyboardInterrupt!")
-'''
